87c5b0c5ec55f3f2cd70bb3ec607d24e1f18d74e
Yuri Costa <eng.yuricosta@gmail.com>
2026-04-02 12:13:11 -0300

    style: change logo image

--- FrontEnd/src/components/LogoBloco.tsx
+++ FrontEnd/src/components/LogoBloco.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import Image from 'next/image';
-import Link from 'next/link'; // CORREÇÃO: Usar Link do Next para navegação rápida
+import Link from 'next/link';
 
 export default function LogoBloco() {
     return (
@@ -12,12 +12,9 @@ export default function LogoBloco() {
                 <div className="flex items-center gap-3">
                     
                     {/* Imagem do Logo */}
-                    {/* CORREÇÃO 1: border-stone-200 -> border-border
-                        Isso faz a borda ser cinza claro no tema light e cinza escuro no dark.
-                    */}
-                    <div className="relative w-10 h-10 rounded-full overflow-hidden border border-border shadow-sm group-hover:border-primary/50 transition-colors">
+                    <div className="relative w-11 h-11 rounded-full overflow-hidden border-s-0 border-border shadow-sm group-hover:border-primary/50 transition-colors">
                         <Image
-                            src="/logo-cazua.jpg"
+                            src="/logo-cazua.png"
                             alt="Logo Cazuá"
                             fill
                             className="object-cover"
@@ -26,15 +23,8 @@ export default function LogoBloco() {
                     </div>
 
                     {/* Texto do Logo */}
-                    {/* CORREÇÃO 2: text-stone-900 -> text-foreground
-                        No Light: Fica quase preto.
-                        No Dark: Fica branco automaticamente.
-                    */}
                     <div className="font-bold text-xl tracking-tighter text-foreground leading-none group-hover:text-primary transition-colors duration-300">
                         cazuá
-                        {/* CORREÇÃO 3: text-stone-400 -> text-muted-foreground
-                            Garante contraste correto em ambos os temas.
-                        */}
                         <span className="text-muted-foreground font-normal">.tech</span>
                     </div>
                 </div>
