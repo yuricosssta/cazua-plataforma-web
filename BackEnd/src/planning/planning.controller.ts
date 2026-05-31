74d76e2be5ab6601189e83e63fdb99ba4e1e7110
Yuri Costa <eng.yuricosta@gmail.com>
2026-05-31 16:46:53 -0300

    feat: add guards on planning controller

--- BackEnd/src/planning/planning.controller.ts
+++ BackEnd/src/planning/planning.controller.ts
@@ -1,20 +1,30 @@
-import { Body, Controller, Get, Param, Post, Query, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, ForbiddenException, Get, Param, Post, Query, Req, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { memoryStorage } from 'multer';
 import { PlanningService } from './planning.service';
 import { ZodValidationPipe } from '../shared/pipe/zod-validation.pipe';
 import { searchPlanningSchema as searchSchema, uploadPlanningSchema, SearchPlanningDto, UploadPlanningDto } from './validations/planning.zod';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('planning')
+@UseGuards(AuthGuard)
 export class PlanningController {
-  constructor(private readonly planningService: PlanningService) {}
+  constructor(private readonly planningService: PlanningService, private readonly configService: ConfigService) { }
 
   @Post('upload')
   @UseInterceptors(FileInterceptor('file', { storage: memoryStorage() }))
   async uploadExcel(
     @UploadedFile() file: Express.Multer.File,
     @Body(new ZodValidationPipe(uploadPlanningSchema)) metadata: UploadPlanningDto,
+    @Req() req: any
   ) {
+    const userEmail = req.user?.email;
+    const superAdminEmail = this.configService.get<string>('SUPER_ADMIN_EMAIL');
+
+    if (userEmail !== superAdminEmail) {
+      throw new ForbiddenException('Acesso negado: Área restrita ao Master Admin.');
+    }
     return this.planningService.uploadFromExcel(file, metadata);
   }
 
